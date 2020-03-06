f363c700a27ccd537c5d2e280d2f7243a3b38e6b
Chaitanya <chaitanyas0101@gmail.com>
2020-03-06 21:48:04 +0530

    added cpp code

--- Linear_search.cpp
+++ Linear_search.cpp
@@ -3,5 +3,23 @@
 using namespace std;
 
 int search(int arr[], int n, int x){
-	
+	int i;
+	for(i = 0;i < n; i++)
+		if (arr[i] == x)
+			return i;
+		return -2;
+}
+
+int main(void)
+{
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int result = search(arr, n, x);
+	if (result == -2)
+	{
+		cout<<"Element is not present";
+	}
+	else{
+		cout<<"Element is present at index"<<result;
+	}
+	return 0;
 }
